7a9765312aabfc58b0cc7aa05c016f2d00c73435
luigipizzeria2 <luigipizzeria22@gmail.com>
2026-03-21 01:27:20 +0100

    website update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => res.send(`<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Luigiho Bot</title>
+<title>Luigiho Bot :D</title>
 <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500;600;700&display=swap" rel="stylesheet">
 <style>
   *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
