63e3f4985985f30f6ba3a3a910bfab360a1dac03
luigipizzeria2 <luigipizzeria22@gmail.com>
2026-03-21 01:16:44 +0100

    websitev1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Bot is running'));
+app.get('/', (req, res) => res.send('I like bread'));
 app.listen(3000, () => console.log('Web server running'));
 
 const {
